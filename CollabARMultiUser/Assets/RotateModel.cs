28d2f87c807ae4a11292a3ce8f3133e9d4f1c71e
nishaankr <nishaankr@gmail.com>
2017-10-08 16:40:11 -0500

    multiuser basic

---
+++ CollabARMultiUser/Assets/RotateModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateModel : MonoBehaviour {
+    public GameObject ModelLoad;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(ModelLoad);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(Vector3.up, 45);
+        }
+        
+    }
+}
