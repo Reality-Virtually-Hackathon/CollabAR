825e239b286f996dd9ccc277cb54e912f6c22952
MDCoen <mau.coen@gmail.com>
2017-10-08 03:10:11 -0500

    lobby and UI done

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-namespace Com.CollabAR.CollabARDemo
+namespace com.CollabAR.CollabARDemo
 {
     public class Launcher : Photon.PunBehaviour
     {
@@ -20,6 +20,10 @@ namespace Com.CollabAR.CollabARDemo
         [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
         public byte MaxPlayersPerRoom = 4;
 
+        [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+        public GameObject controlPanel;
+        [Tooltip("The UI Label to inform the user that the connection is in progress")]
+        public GameObject progressLabel;
         #endregion
 
 
@@ -66,7 +70,8 @@ namespace Com.CollabAR.CollabARDemo
         /// </summary>
         void Start()
         {
-            Connect();
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
         }
 
 
@@ -84,7 +89,8 @@ namespace Com.CollabAR.CollabARDemo
         public void Connect()
         {
 
-
+            progressLabel.SetActive(true);
+            controlPanel.SetActive(false);
             // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
             if (PhotonNetwork.connected)
             {
@@ -116,6 +122,8 @@ namespace Com.CollabAR.CollabARDemo
 
         public override void OnDisconnectedFromPhoton()
         {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
             Debug.LogWarning("DemoAnimator/Launcher: OnDisconnectedFromPhoton() was called by PUN");
         }
 
---
+++ Assets/Scripts/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ViewModel : MonoBehaviour 
+{
+   // public Text buttonText; //prob don't need this button
+    public GameObject outerLung1,outerLung2;//our lung components
+    public Slider slider;
+    void Update()
+    {
+
+        if (slider.value == 1)
+        {
+            outerLung1.GetComponent<Renderer>().enabled = false;
+            outerLung2.GetComponent<Renderer>().enabled = false;
+        }
+        else
+        {
+            outerLung1.GetComponent<Renderer>().enabled = true;
+            outerLung2.GetComponent<Renderer>().enabled = true;
+        }
+
+    }
+  /*  public void Toggle_Changed(bool newValue)
+    {
+        cube.SetActive(newValue);
+        slider.interactable = newValue; //so when it is changed, activates or deactivates the object
+    }
+    */
+
+}
